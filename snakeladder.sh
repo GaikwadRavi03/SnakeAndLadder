4ec406ab4c3b5f64a02615d0e3bd3a4eac655639
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-15 18:26:23 -0500

    Add: Check for option NoPlay, Ladder and Snake

--- snakeladder.sh
+++ snakeladder.sh
@@ -2,7 +2,19 @@
 echo "snake and ladder game"
 
 PLAYER=1
+NUMBER=0
+LADDER=1
+SNAKE=2
 position=0
 
 die=$((RANDOM%6+1))
 echo $die
+
+case $random in 
+	$NUMBER )
+	position=0;;
+	$LADDER )
+	position=$(($position + $die));;
+	$SNAKE )
+	position=$(($position - $die));;
+esac
